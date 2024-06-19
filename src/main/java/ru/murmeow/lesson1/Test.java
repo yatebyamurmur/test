2d6fae31e7ef1fd31809351d8137431ffe045b44
Brilliance <myawmurmeow@gmail.com>
2024-06-20 01:08:57 +0400

    privet

---
+++ src/main/java/ru/murmeow/lesson1/Test.java
@@ -0,0 +1,9 @@
+package ru.murmeow.lesson1;
+public class Test {
+
+    int x = 10;
+    long l = 12222222223433341L;
+    double d = 1.12312312412421;
+    char c = 'z';
+    String s = "hihihi";
+}
